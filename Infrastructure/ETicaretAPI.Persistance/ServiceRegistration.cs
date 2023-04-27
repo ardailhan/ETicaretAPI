bf2fb791ca016f35598e1eab5ac1e83523c35fee
Arda İlhan <ardailhan7@gmail.com>
2023-04-27 15:31:39 +0300

    entities and service registration added

---
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, IProductService>();
+        }
+    }
+}
