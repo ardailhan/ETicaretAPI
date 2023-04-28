c488f7bf7cc4a62f219f3b2aa2c7e482d83181af
ardailhan <ardailhan_@outlook.com>
2023-04-28 20:19:14 +0300

    Entities Added

--- Core/ETicaretAPI.Domain/Entities/Customer.cs
+++ Core/ETicaretAPI.Domain/Entities/Customer.cs
@@ -1,6 +1,5 @@
 ﻿using ETicaretAPI.Domain.Entities.Common;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +7,9 @@ using System.Threading.Tasks;
 
 namespace ETicaretAPI.Domain.Entities
 {
-    public class Product : BaseEntity
+    public class Customer : BaseEntity
     {
         public string Name { get; set; }
-        public int Stock { get; set; }
-        public long Price { get; set; }
-
         public ICollection<Order> Orders { get; set; }
     }
 }
