f9c965e231f8184d2b2fd313f4fc07f5d4897dfd
ardailhan <ardailhan_@outlook.com>
2023-05-07 11:58:00 +0300

    FileService and FileServiceRegistration

---
+++ Core/ETicaretAPI.Application/Services/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ETicaretAPI.Application.Services
+{
+    public interface IFileService
+    {
+        Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files);
+        Task<string> FİleRenameAsync(string fileName);
+        Task<bool> CopyFileAsync(string path, IFormFile file);
+    }
+}
