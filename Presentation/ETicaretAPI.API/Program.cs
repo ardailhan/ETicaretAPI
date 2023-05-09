11d756f9228b5b7c1f6575a17d0ae15bc396de79
ardailhan <ardailhan_@outlook.com>
2023-05-09 14:52:28 +0300

    just started to CQRS pattern and using MediatR lib

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,4 +1,5 @@
-﻿using ETicaretAPI.Application.Validators.Products;
+﻿using ETicaretAPI.Application;
+using ETicaretAPI.Application.Validators.Products;
 using ETicaretAPI.Infrastructure;
 using ETicaretAPI.Infrastructure.Filters;
 using ETicaretAPI.Infrastructure.Services.Storage.Azure;
@@ -11,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPersistanceServices();
 builder.Services.AddInfrastructureServices();
+builder.Services.AddApplicationServices();
 
 //builder.Services.AddStorage<LocalStorage>();
 builder.Services.AddStorage<AzureStorage>();
