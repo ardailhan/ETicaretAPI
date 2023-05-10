f72f261f6073c6540397dd8e45608f32640f3dc5
ardailhan <ardailhan_@outlook.com>
2023-05-10 20:28:48 +0300

    identity mechanism and adding a new user

--- Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using ETicaretAPI.Application.Repositories;
 using ETicaretAPI.Persistance.Repositories;
+using ETicaretAPI.Domain.Entities.Identity;
 
 namespace ETicaretAPI.Persistance
 {
@@ -17,6 +18,15 @@ namespace ETicaretAPI.Persistance
         public static void AddPersistanceServices(this IServiceCollection services)
         {
             services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            }).AddEntityFrameworkStores<ETicaretAPIDbContext>();
+
             services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
             services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
             services.AddScoped<IOrderReadRepository, OrderReadRepository>();
