8c34bbfc3b231d984089687f88d6421fb187ccbd
ardailhan <ardailhan_@outlook.com>
2023-05-26 18:55:57 +0300

    errors fixed  HasRolePermissionToEndpointAsync method in UserService.

--- Infrastructure/ETicaretAPI.Persistance/Services/UserService.cs
+++ Infrastructure/ETicaretAPI.Persistance/Services/UserService.cs
@@ -100,8 +100,9 @@ namespace ETicaretAPI.Persistance.Services
         public async Task<string[]> GetRolesToUserAsync(string userIdOrName)
         {
             AppUser user = await _userManager.FindByIdAsync(userIdOrName);
-            if (user != null)
+            if (user == null)
                 user = await _userManager.FindByNameAsync(userIdOrName);
+
             if (user != null)
             {
                 var userRoles = await _userManager.GetRolesAsync(user);
@@ -113,13 +114,16 @@ namespace ETicaretAPI.Persistance.Services
         public async Task<bool> HasRolePermissionToEndpointAsync(string name, string code)
         {
             var userRoles = await GetRolesToUserAsync(name);
-            if (!userRoles.Any()) return false;
+
+            if (!userRoles.Any())
+                return false;
 
             Endpoint? endpoint = await _endpointReadRepository.Table
-                 .Include(e => e.Roles)
-                 .FirstOrDefaultAsync(e => e.Code == code);
+                     .Include(e => e.Roles)
+                     .FirstOrDefaultAsync(e => e.Code == code);
 
-            if (endpoint == null) return false;
+            if (endpoint == null)
+                return false;
 
             var hasRole = false;
             var endpointRoles = endpoint.Roles.Select(r => r.Name);
@@ -138,14 +142,16 @@ namespace ETicaretAPI.Persistance.Services
             //    else
             //        break;
             //}
+
             //return hasRole;
 
             foreach (var userRole in userRoles)
             {
                 foreach (var endpointRole in endpointRoles)
-                    if(userRole == endpointRole)
+                    if (userRole == endpointRole)
                         return true;
             }
+
             return false;
         }
 
